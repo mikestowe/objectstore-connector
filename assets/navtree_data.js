bccd48eecbe0807491f1722de43e907ae6b2d2bf
MuleSoft <code-contrib@mulesoft.com>
2011-12-01 08:25:15 -0800

    Updating documentation

---
+++ assets/navtree_data.js
@@ -0,0 +1,11 @@
+var NAVTREE_DATA =
+[ [ "org.mule.modules", "org/mule/modules/package-summary.html", [ [ "Classes", null, [ [ "ObjectStoreModule", "org/mule/modules/ObjectStoreModule.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mule.modules.config", "org/mule/modules/config/package-summary.html", [ [ "Classes", null, [ [ "ObjectStoreModuleCapabilitiesAdapter", "org/mule/modules/config/ObjectStoreModuleCapabilitiesAdapter.html", null, "" ], [ "ObjectStoreModuleInjectAdapter", "org/mule/modules/config/ObjectStoreModuleInjectAdapter.html", null, "" ], [ "ObjectStoreModuleLifecycleAdapter", "org/mule/modules/config/ObjectStoreModuleLifecycleAdapter.html", null, "" ], [ "RemoveMessageProcessor", "org/mule/modules/config/RemoveMessageProcessor.html", null, "" ], [ "RetrieveMessageProcessor", "org/mule/modules/config/RetrieveMessageProcessor.html", null, "" ], [ "StoreMessageProcessor", "org/mule/modules/config/StoreMessageProcessor.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mule.modules.config.spring", "org/mule/modules/config/spring/package-summary.html", [ [ "Classes", null, [ [ "ObjectStoreModuleConfigDefinitionParser", "org/mule/modules/config/spring/ObjectStoreModuleConfigDefinitionParser.html", null, "" ], [ "ObjectStoreModuleNamespaceHandler", "org/mule/modules/config/spring/ObjectStoreModuleNamespaceHandler.html", null, "" ], [ "RemoveDefinitionParser", "org/mule/modules/config/spring/RemoveDefinitionParser.html", null, "" ], [ "RetrieveDefinitionParser", "org/mule/modules/config/spring/RetrieveDefinitionParser.html", null, "" ], [ "StoreDefinitionParser", "org/mule/modules/config/spring/StoreDefinitionParser.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ java/lists.js
@@ -0,0 +1,21 @@
+var DATA = [
+      { id:0, label:"org.mule.modules", link:"java/org/mule/modules/package-summary.html", type:"package" },
+      { id:1, label:"org.mule.modules.ObjectStoreModule", link:"java/org/mule/modules/ObjectStoreModule.html", type:"class" },
+      { id:2, label:"org.mule.modules.config", link:"java/org/mule/modules/config/package-summary.html", type:"package" },
+      { id:3, label:"org.mule.modules.config.ObjectStoreModuleCapabilitiesAdapter", link:"java/org/mule/modules/config/ObjectStoreModuleCapabilitiesAdapter.html", type:"class" },
+      { id:4, label:"org.mule.modules.config.ObjectStoreModuleInjectAdapter", link:"java/org/mule/modules/config/ObjectStoreModuleInjectAdapter.html", type:"class" },
+      { id:5, label:"org.mule.modules.config.ObjectStoreModuleLifecycleAdapter", link:"java/org/mule/modules/config/ObjectStoreModuleLifecycleAdapter.html", type:"class" },
+      { id:6, label:"org.mule.modules.config.RemoveMessageProcessor", link:"java/org/mule/modules/config/RemoveMessageProcessor.html", type:"class" },
+      { id:7, label:"org.mule.modules.config.RetrieveMessageProcessor", link:"java/org/mule/modules/config/RetrieveMessageProcessor.html", type:"class" },
+      { id:8, label:"org.mule.modules.config.StoreMessageProcessor", link:"java/org/mule/modules/config/StoreMessageProcessor.html", type:"class" },
+      { id:9, label:"org.mule.modules.config.spring", link:"java/org/mule/modules/config/spring/package-summary.html", type:"package" },
+      { id:10, label:"org.mule.modules.config.spring.ObjectStoreModuleConfigDefinitionParser", link:"java/org/mule/modules/config/spring/ObjectStoreModuleConfigDefinitionParser.html", type:"class" },
+      { id:11, label:"org.mule.modules.config.spring.ObjectStoreModuleNamespaceHandler", link:"java/org/mule/modules/config/spring/ObjectStoreModuleNamespaceHandler.html", type:"class" },
+      { id:12, label:"org.mule.modules.config.spring.RemoveDefinitionParser", link:"java/org/mule/modules/config/spring/RemoveDefinitionParser.html", type:"class" },
+      { id:13, label:"org.mule.modules.config.spring.RetrieveDefinitionParser", link:"java/org/mule/modules/config/spring/RetrieveDefinitionParser.html", type:"class" },
+      { id:14, label:"org.mule.modules.config.spring.StoreDefinitionParser", link:"java/org/mule/modules/config/spring/StoreDefinitionParser.html", type:"class" },
+      { id:15, label:"remove", link:"mule/objectstore.html#remove", type:"method" },
+      { id:16, label:"retrieve", link:"mule/objectstore.html#retrieve", type:"method" },
+      { id:17, label:"store", link:"mule/objectstore.html#store", type:"method" }
+
+    ];
