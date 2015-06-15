b3372ef7c0cec9a32a9b0d2307eeedce613ee4dc
Tejaswi Pydimarri <pnvtejaswi@gmail.com>
2015-06-15 12:13:17 +0530

    JIRA Story CLDCONNECT-2369 - https://www.mulesoft.org/jira/browse/CLDCONNECT-2369 1. Connector migrated to devkit 3.7.0-SNAPSHOT 2. Refactored the connector by adding ObjectStoreConfiguration class to contain configurable global elements.

--- src/main/java/org/mule/modules/objectstore/ObjectStoreModule.java
+++ src/main/java/org/mule/modules/objectstore/ObjectStoreModule.java
@@ -1,95 +1,43 @@
 /**
- * Copyright (c) MuleSoft, Inc. All rights reserved. http://www.mulesoft.com
- *
- * The software in this package is published under the terms of the CPAL v1.0
- * license, a copy of which has been included with this distribution in the
- * LICENSE.md file.
+ * (c) 2003-2015 MuleSoft, Inc. The software in this package is
+ * published under the terms of the CPAL v1.0 license, a copy of which
+ * has been included with this distribution in the LICENSE.md file.
  */
 
-package org.mule.modules;
-
-import java.io.Serializable;
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.locks.Lock;
-
-import javax.annotation.PostConstruct;
-import javax.inject.Inject;
+package org.mule.modules.objectstore;
 
 import org.mule.api.MuleContext;
 import org.mule.api.MuleMessage;
-import org.mule.api.annotations.Configurable;
+import org.mule.api.annotations.Config;
 import org.mule.api.annotations.Module;
 import org.mule.api.annotations.Processor;
 import org.mule.api.annotations.param.Default;
 import org.mule.api.annotations.param.Optional;
 import org.mule.api.config.MuleProperties;
-import org.mule.api.context.MuleContextAware;
 import org.mule.api.registry.Registry;
-import org.mule.api.store.ListableObjectStore;
-import org.mule.api.store.ObjectAlreadyExistsException;
-import org.mule.api.store.ObjectDoesNotExistException;
-import org.mule.api.store.ObjectStore;
-import org.mule.api.store.ObjectStoreException;
-import org.mule.api.store.ObjectStoreManager;
+import org.mule.api.store.*;
 import org.mule.api.transport.PropertyScope;
 import org.mule.util.StringUtils;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.locks.Lock;
 
 /**
  * Generic module for accessing Object Stores.
  * <p/>
- * Can be used with Mule default implementations or one can be passed via ref. It allows to store,
- * retrieve and remove objects from the store.
+ * Can be used with Mule default implementations or one can be passed via ref. It allows to store, retrieve and remove objects from the store.
  *
  * @author MuleSoft, Inc.
  */
 @Module(name = "objectstore", schemaVersion = "1.0", friendlyName = "ObjectStore")
 public class ObjectStoreModule {
-    /**
-     * Name of the partition in the default in-memory or persistent object stores (this argument has no
-     * meaning if the object store is passed by ref using objectStore-ref)
-     */
-    @Configurable
-    @Optional
-    private String partition;
 
-    /**
-     * Specified whenever the required store needs to be persistent or not (this argument has no
-     * meaning if the object store is passed by ref using objectStore-ref or no partition name is defined)
-     */
-    @Configurable
-    @Optional
-    @Default("false")
-    private boolean persistent;
-
-    /**
-     * Reference to an Object Store bean. This is optional and if it is not specified then the default
-     * in-memory or persistent store will be used.
-     */
-    @Configurable
-    @Optional
-    private ObjectStore<Serializable> objectStore;
-
-    /**
-     * TimeToLive for stored values in milliseconds. MaxEntries and ExpirationInterval are mandatory for using this param.
-     */
-    @Configurable
-    @Optional
-    private Integer entryTtl;
-
-    /**
-     * Specifies the max number of entries. EntryTTL and ExpirationInterval are mandatory for using this param.
-     */
-    @Configurable
-    @Optional
-    private Integer maxEntries;
-
-    /**
-     * Specifies the expiration check interval in milliseconds. EntryTTL and MaxEntries are mandatory for using this param.
-     */
-    @Configurable
-    @Optional
-    private Integer expirationInterval;
+    @Config
+    private ObjectStoreConfiguration configuration;
 
     @Inject
     private Registry registry;
@@ -101,20 +49,25 @@ public class ObjectStoreModule {
     private MuleContext muleContext = null;
 
     private String sharedObjectStoreLockId = null;
+    private String partition;
+    private boolean persistent;
+    private ObjectStore<Serializable> objectStore;
+    private Integer entryTtl;
+    private Integer maxEntries;
+    private Integer expirationInterval;
 
     @PostConstruct
     public void init() {
         if (objectStore == null) {
-            if(StringUtils.isNotEmpty(partition)) {
-                if(entryTtl != null && maxEntries != null && expirationInterval != null) {
+            if (StringUtils.isNotEmpty(partition)) {
+                if (entryTtl != null && maxEntries != null && expirationInterval != null) {
                     objectStore = objectStoreManager.getObjectStore(partition, persistent, maxEntries, entryTtl, expirationInterval);
-                }
-                else {
+                } else {
                     objectStore = objectStoreManager.getObjectStore(partition, persistent);
                 }
             }
 
-            if(objectStore == null) {
+            if (objectStore == null) {
                 objectStore = registry.lookupObject(MuleProperties.DEFAULT_USER_OBJECT_STORE_NAME);
             }
 
@@ -133,20 +86,21 @@ public class ObjectStoreModule {
      * <p/>
      * {@sample.xml ../../../doc/mule-module-objectstore.xml.sample objectstore:store}
      *
-     * @param key       The identifier of the object to store
-     * @param value     The object to store. If you want this to be the payload then use value-ref="#[payload]".
-     * @param overwrite True if you want to overwrite the existing object.
+     * @param key
+     *            The identifier of the object to store
+     * @param value
+     *            The object to store. If you want this to be the payload then use value-ref="#[payload]".
+     * @param overwrite
+     *            True if you want to overwrite the existing object.
      * @throws org.mule.api.store.ObjectStoreException
-     *          if the given key cannot be stored or is <code>null</code>.
+     *             if the given key cannot be stored or is <code>null</code>.
      * @throws org.mule.api.store.ObjectStoreNotAvaliableException
-     *          if the store is not available or any other
-     *          implementation-specific error occured.
+     *             if the store is not available or any other implementation-specific error occured.
      * @throws org.mule.api.store.ObjectAlreadyExistsException
-     *          if an attempt is made to store an object for a key
-     *          that already has an object associated. Only thrown if overwrite is false.
+     *             if an attempt is made to store an object for a key that already has an object associated. Only thrown if overwrite is false.
      */
     @Processor
-    public void store(String key, Serializable value, @Optional @Default("false") boolean overwrite) throws ObjectStoreException {
+    public void store(String key, Serializable value, @Default("false") boolean overwrite) throws ObjectStoreException {
         Lock lock = muleContext.getLockFactory().createLock(sharedObjectStoreLockId);
         lock.lock();
         try {
@@ -168,23 +122,23 @@ public class ObjectStoreModule {
      * <p/>
      * {@sample.xml ../../../doc/mule-module-objectstore.xml.sample objectstore:dual-store}
      *
-     * @param key       The identifier of the object to store
-     * @param value     The object to store. If you want this to be the payload then use value-ref="#[payload]".
-     * @param overwrite True if you want to overwrite the existing object.
+     * @param key
+     *            The identifier of the object to store
+     * @param value
+     *            The object to store. If you want this to be the payload then use value-ref="#[payload]".
+     * @param overwrite
+     *            True if you want to overwrite the existing object.
      * @throws org.mule.api.store.ObjectStoreException
-     *          if the given key cannot be stored or is <code>null</code>.
+     *             if the given key cannot be stored or is <code>null</code>.
      * @throws org.mule.api.store.ObjectStoreNotAvaliableException
-     *          if the store is not available or any other
-     *          implementation-specific error occured.
+     *             if the store is not available or any other implementation-specific error occured.
      * @throws org.mule.api.store.ObjectAlreadyExistsException
-     *          if an attempt is made to store an object for a key
-     *          that already has an object associated. Only thrown if overwrite is false.
+     *             if an attempt is made to store an object for a key that already has an object associated. Only thrown if overwrite is false.
      */
     @Processor
-    public void dualStore(String key, Serializable value, @Optional @Default("false") boolean overwrite)
-            throws ObjectStoreException {
+    public void dualStore(String key, Serializable value, @Default("false") boolean overwrite) throws ObjectStoreException {
 
-        //For rollback purposes
+        // For rollback purposes
         Serializable previousValue = null;
         Lock lock = muleContext.getLockFactory().createLock(sharedObjectStoreLockId);
         lock.lock();
@@ -225,24 +179,27 @@ public class ObjectStoreModule {
      * <p/>
      * {@sample.xml ../../../doc/mule-module-objectstore.xml.sample objectstore:retrieve}
      *
-     * @param key          The identifier of the object to retrieve.
-     * @param defaultValue The default value if the key does not exists.
-     * @param targetProperty The Mule Message property where the retrieved value will be stored
-     * @param targetScope  The Mule Message property scope, only used when targetProperty is specified
-     * @param muleMessage  Injected Mule Message
-     * @return The object associated with the given key. If no object for the given key was found
-     *         this method throws an {@link org.mule.api.store.ObjectDoesNotExistException}.
-     * @throws ObjectStoreException if the given key is <code>null</code>.
+     * @param key
+     *            The identifier of the object to retrieve.
+     * @param defaultValue
+     *            The default value if the key does not exists.
+     * @param targetProperty
+     *            The Mule Message property where the retrieved value will be stored
+     * @param targetScope
+     *            The Mule Message property scope, only used when targetProperty is specified
+     * @param muleMessage
+     *            Injected Mule Message
+     * @return The object associated with the given key. If no object for the given key was found this method throws an {@link org.mule.api.store.ObjectDoesNotExistException}.
+     * @throws ObjectStoreException
+     *             if the given key is <code>null</code>.
      * @throws org.mule.api.store.ObjectStoreNotAvaliableException
-     *                              if the store is not  available or any other
-     *                              implementation-specific error occured.
+     *             if the store is not available or any other implementation-specific error occured.
      * @throws org.mule.api.store.ObjectDoesNotExistException
-     *                              if no value for the given key was previously stored.
+     *             if no value for the given key was previously stored.
      */
     @Processor
-    public Object retrieve(String key, @Optional Object defaultValue, @Optional String targetProperty,
-                           @Optional @Default("INVOCATION") MulePropertyScope targetScope,
-                           MuleMessage muleMessage) throws ObjectStoreException {
+    public Object retrieve(String key, @Optional Object defaultValue, @Optional String targetProperty, @Default("INVOCATION") MulePropertyScope targetScope, MuleMessage muleMessage)
+            throws ObjectStoreException {
         Object ret = null;
         try {
             ret = objectStore.retrieve(key);
@@ -266,18 +223,18 @@ public class ObjectStoreModule {
      * <p/>
      * {@sample.xml ../../../doc/mule-module-objectstore.xml.sample objectstore:remove}
      *
-     * @param key The identifier of the object to remove.
-     * @param ignoreNotExists Indicates if the operation will ignore NotExistsException from ObjectStore.
-     * @return The object that was previously stored for the given key.  If the key does not exist and
-     * ignoreNotExists is true the operation will return a null object.
-     * @throws ObjectStoreException if the given key is <code>null</code> or if the store is not
-     *                              available or any other implementation-specific error occurred
+     * @param key
+     *            The identifier of the object to remove.
+     * @param ignoreNotExists
+     *            Indicates if the operation will ignore NotExistsException from ObjectStore.
+     * @return The object that was previously stored for the given key. If the key does not exist and ignoreNotExists is true the operation will return a null object.
+     * @throws ObjectStoreException
+     *             if the given key is <code>null</code> or if the store is not available or any other implementation-specific error occurred
      * @throws org.mule.api.store.ObjectDoesNotExistException
-     *                              if no value for the given key was previously stored.
+     *             if no value for the given key was previously stored.
      */
     @Processor
-    public Object remove(String key, @Optional @Default("false") boolean ignoreNotExists)
-            throws ObjectStoreException {
+    public Object remove(String key, @Default("false") boolean ignoreNotExists) throws ObjectStoreException {
         Lock lock = muleContext.getLockFactory().createLock(sharedObjectStoreLockId);
         lock.lock();
         try {
@@ -301,7 +258,8 @@ public class ObjectStoreModule {
      * {@sample.xml ../../../doc/mule-module-objectstore.xml.sample objectstore:all-keys}
      *
      * @return a java.util.List with all the keys in the store.
-     * @throws ObjectStoreException if an exception occurred while collecting the list of all keys.
+     * @throws ObjectStoreException
+     *             if an exception occurred while collecting the list of all keys.
      */
     @Processor
     public List<Serializable> allKeys() throws ObjectStoreException {
@@ -317,9 +275,11 @@ public class ObjectStoreModule {
      *
      * {@sample.xml ../../../doc/mule-module-objectstore.xml.sample objectstore:contains}
      *
-     * @param key The identifier of the object to validate.
+     * @param key
+     *            The identifier of the object to validate.
      * @return true if the object store contains the key, or false if it doesn't.
-     * @throws ObjectStoreException if the provided key is null.
+     * @throws ObjectStoreException
+     *             if the provided key is null.
      */
     @Processor
     public boolean contains(String key) throws ObjectStoreException {
@@ -342,7 +302,7 @@ public class ObjectStoreModule {
     private void silentlyDelete(String key) {
         try {
             objectStore.remove(key);
-        } catch(Exception ex) {
+        } catch (Exception ex) {
 
         }
     }
@@ -355,63 +315,29 @@ public class ObjectStoreModule {
         this.objectStoreManager = objectStoreManager;
     }
 
-    public void setPartition(String partition) {
-        this.partition = partition;
-    }
-
-    public void setPersistent(boolean persistent) {
-        this.persistent = persistent;
-    }
-
-    public void setObjectStore(ObjectStore<Serializable> objectStore) {
-        this.objectStore = objectStore;
-    }
-
     public void setRegistry(Registry registry) {
         this.registry = registry;
-    }
-
-    public String getPartition() {
-        return partition;
-    }
-
-    public boolean isPersistent() {
-        return persistent;
-    }
-
-    public ObjectStore<Serializable> getObjectStore() {
-        return objectStore;
     }
 
     public Registry getRegistry() {
         return registry;
     }
 
-    public Integer getEntryTtl() {
-        return entryTtl;
-    }
-
-    public void setEntryTtl(Integer entryTtl) {
-        this.entryTtl = entryTtl;
-    }
-
-    public Integer getMaxEntries() {
-        return maxEntries;
-    }
-
-    public void setMaxEntries(Integer maxEntries) {
-        this.maxEntries = maxEntries;
-    }
-
-    public Integer getExpirationInterval() {
-        return expirationInterval;
-    }
-
-    public void setExpirationInterval(Integer expirationInterval) {
-        this.expirationInterval = expirationInterval;
-    }
-
     public void setMuleContext(MuleContext context) {
         this.muleContext = context;
+    }
+
+    public ObjectStoreConfiguration getConfiguration() {
+        return configuration;
+    }
+
+    public void setConfiguration(ObjectStoreConfiguration configuration) {
+        this.configuration = configuration;
+        this.partition = configuration.getPartition();
+        this.persistent = configuration.getPersistent();
+        this.objectStore = configuration.getObjectStore();
+        this.entryTtl = configuration.getEntryTtl();
+        this.maxEntries = configuration.getMaxEntries();
+        this.expirationInterval = configuration.getExpirationInterval();
     }
 }
--- src/test/java/org/mule/modules/objectstore/ObjectStoreModuleTest.java
+++ src/test/java/org/mule/modules/objectstore/ObjectStoreModuleTest.java
@@ -1,14 +1,10 @@
 /**
- * Copyright (c) MuleSoft, Inc. All rights reserved. http://www.mulesoft.com
- *
- * The software in this package is published under the terms of the CPAL v1.0
- * license, a copy of which has been included with this distribution in the
- * LICENSE.md file.
+ * (c) 2003-2015 MuleSoft, Inc. The software in this package is
+ * published under the terms of the CPAL v1.0 license, a copy of which
+ * has been included with this distribution in the LICENSE.md file.
  */
 
-package org.mule.modules;
-
-import java.util.List;
+package org.mule.modules.objectstore;
 
 import org.junit.After;
 import org.junit.Rule;
@@ -22,12 +18,11 @@ import org.mule.construct.Flow;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.transport.NullPayload;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertFalse;
-import static junit.framework.Assert.assertTrue;
+import java.util.List;
 
-public class ObjectStoreModuleTest extends FunctionalTestCase
-{
+import static junit.framework.Assert.*;
+
+public class ObjectStoreModuleTest extends FunctionalTestCase {
 
     private static final String OBJECTSTORE_KEY = "mykey";
     private static final String OBJECTSTORE_VALUE = "mulesoft";
@@ -35,8 +30,7 @@ public class ObjectStoreModuleTest extends FunctionalTestCase
     private static final String OBJECTSTORE_CONFIG_NAME = "test-partition";
 
     @Override
-    protected String getConfigResources()
-    {
+    protected String getConfigResources() {
         return "mule-config.xml";
     }
 
@@ -44,62 +38,56 @@ public class ObjectStoreModuleTest extends FunctionalTestCase
     public ExpectedException thrownException = ExpectedException.none();
 
     @Test
-    public void testStore() throws Exception
-    {
+    public void testStore() throws Exception {
         runFlowWithPayload("store", "mulesoft2");
         runFlowWithPayload("store", OBJECTSTORE_VALUE);
 
         ObjectStoreModule module = muleContext.getRegistry().lookupObject(OBJECTSTORE_CONFIG_NAME);
-        String value = (String)module.getObjectStoreManager().getObjectStore(OBJECTSTORE_PARTITION).retrieve(OBJECTSTORE_KEY);
+        String value = (String) module.getObjectStoreManager().getObjectStore(OBJECTSTORE_PARTITION).retrieve(OBJECTSTORE_KEY);
 
         assertEquals(OBJECTSTORE_VALUE, value);
     }
 
     @Test
-    public void testDualStore() throws Exception
-    {
+    public void testDualStore() throws Exception {
         runFlowWithPayload("dualStore", OBJECTSTORE_VALUE);
 
         ObjectStoreModule module = muleContext.getRegistry().lookupObject(OBJECTSTORE_CONFIG_NAME);
-        String value1 = (String)module.getObjectStoreManager().getObjectStore(OBJECTSTORE_PARTITION).retrieve(OBJECTSTORE_KEY);
-        String value2 = (String)module.getObjectStoreManager().getObjectStore(OBJECTSTORE_PARTITION).retrieve(OBJECTSTORE_VALUE);
+        String value1 = (String) module.getObjectStoreManager().getObjectStore(OBJECTSTORE_PARTITION).retrieve(OBJECTSTORE_KEY);
+        String value2 = (String) module.getObjectStoreManager().getObjectStore(OBJECTSTORE_PARTITION).retrieve(OBJECTSTORE_VALUE);
 
         assertEquals(OBJECTSTORE_VALUE, value1);
         assertEquals(OBJECTSTORE_KEY, value2);
     }
 
     @Test
-    public void testDualStoreOverwrite() throws Exception
-    {
+    public void testDualStoreOverwrite() throws Exception {
         ObjectStoreModule module = muleContext.getRegistry().lookupObject(OBJECTSTORE_CONFIG_NAME);
         module.store(OBJECTSTORE_KEY, "myKeyValue", true);
         module.store(OBJECTSTORE_VALUE, "myKeyValue2", true);
         runFlowWithPayload("dualStoreOverwrite", OBJECTSTORE_VALUE);
 
-        String value1 = (String)module.getObjectStoreManager().getObjectStore(OBJECTSTORE_PARTITION).retrieve(OBJECTSTORE_KEY);
-        String value2 = (String)module.getObjectStoreManager().getObjectStore(OBJECTSTORE_PARTITION).retrieve(OBJECTSTORE_VALUE);
+        String value1 = (String) module.getObjectStoreManager().getObjectStore(OBJECTSTORE_PARTITION).retrieve(OBJECTSTORE_KEY);
+        String value2 = (String) module.getObjectStoreManager().getObjectStore(OBJECTSTORE_PARTITION).retrieve(OBJECTSTORE_VALUE);
 
         assertEquals(OBJECTSTORE_VALUE, value1);
         assertEquals(OBJECTSTORE_KEY, value2);
     }
 
     @Test
-    public void testRetrieve() throws Exception
-    {
+    public void testRetrieve() throws Exception {
         runFlowWithPayload("store", OBJECTSTORE_VALUE);
 
-        assertEquals(OBJECTSTORE_VALUE, runFlow("retrieve"));
+        assertEquals(OBJECTSTORE_VALUE, runFlow("retrieve").getMessage().getPayload());
     }
 
     @Test
-    public void testRetrieveDefaultValue() throws Exception
-    {
-        assertEquals("muleion", runFlow("retrieveDefaultValue"));
+    public void testRetrieveDefaultValue() throws Exception {
+        assertEquals("muleion", runFlow("retrieveDefaultValue").getMessage().getPayload());
     }
 
     @Test
-    public void testRetrieveToInvocationVariable() throws Exception
-    {
+    public void testRetrieveToInvocationVariable() throws Exception {
         runFlowWithPayload("store", OBJECTSTORE_VALUE);
         Flow flow = lookupFlowConstruct("retrieveVariable");
         MuleEvent event = getTestEvent(null);
@@ -109,39 +97,35 @@ public class ObjectStoreModuleTest extends FunctionalTestCase
     }
 
     @Test
-    public void testRemove() throws Exception
-    {
+    public void testRemove() throws Exception {
         runFlowWithPayload("store", OBJECTSTORE_VALUE);
-        Object removedObject = runFlow("remove");
+        Object removedObject = runFlow("remove").getMessage().getPayload();
 
         assertEquals(OBJECTSTORE_VALUE, removedObject);
     }
 
     @Test
-    public void testRemoveNotExists() throws Exception
-    {
-        Object removedObject = runFlow("removeNotExists");
+    public void testRemoveNotExists() throws Exception {
+        Object removedObject = runFlow("removeNotExists").getMessage().getPayload();
 
         assertTrue(removedObject instanceof NullPayload);
     }
 
     @Test
-    public void testAllKeys() throws Exception
-    {
+    public void testAllKeys() throws Exception {
         runFlowWithPayload("store", OBJECTSTORE_VALUE);
-        Object keys = runFlow("allKeys");
-        
+        Object keys = runFlow("allKeys").getMessage().getPayload();
+
         assertTrue(keys instanceof List);
         assertEquals(1, ((List<?>) keys).size());
         assertEquals(OBJECTSTORE_KEY, ((List<?>) keys).get(0));
-        
+
         // Remove keys from persistent store for next tests!
         runFlow("remove");
     }
 
     @Test
-    public void testRetrieveExpirable() throws Exception
-    {
+    public void testRetrieveExpirable() throws Exception {
         runFlowWithPayload("testStoreExpirable", OBJECTSTORE_VALUE);
         Thread.sleep(2000L);
 
@@ -167,18 +151,18 @@ public class ObjectStoreModuleTest extends FunctionalTestCase
 
     @After
     public void tearDown() throws ObjectStoreException {
-        //clean object store
+        // clean object store
         ObjectStoreModule module = muleContext.getRegistry().lookupObject(OBJECTSTORE_CONFIG_NAME);
         removeFromObjectStore(module, OBJECTSTORE_KEY, OBJECTSTORE_VALUE);
     }
-    
+
     /**
-    * Run the flow specified by name
-    *
-    * @param flowName The name of the flow to run
-    */
-    protected <T> Object runFlowWithPayload(String flowName, T payload) throws Exception
-    {
+     * Run the flow specified by name
+     *
+     * @param flowName
+     *            The name of the flow to run
+     */
+    protected <T> Object runFlowWithPayload(String flowName, T payload) throws Exception {
         Flow flow = lookupFlowConstruct(flowName);
         MuleEvent event = getTestEvent(payload);
         MuleEvent responseEvent = flow.process(event);
@@ -186,39 +170,19 @@ public class ObjectStoreModuleTest extends FunctionalTestCase
     }
 
     /**
-    * Run the flow specified by name
-    *
-    * @param flowName The name of the flow to run
-    */
-    protected Object runFlow(String flowName) throws Exception
-    {
-        Flow flow = lookupFlowConstruct(flowName);
-        MuleEvent event = getTestEvent(null);
-        MuleEvent responseEvent = flow.process(event);
-
-        return responseEvent.getMessage().getPayload();
-    }
-
-    /**
-     * Retrieve a flow by name from the registry
-     *
-     * @param name Name of the flow to retrieve
-     */
-    protected Flow lookupFlowConstruct(String name) throws Exception {
-        return (Flow) getTestService().getMuleContext().getRegistry().lookupFlowConstruct(name);
-    }
-
-    /**
      * Removes the specified keys from the Object Store, using the test partition
-     * @param module Object Store module
-     * @param keys list of keys to be removed
+     * 
+     * @param module
+     *            Object Store module
+     * @param keys
+     *            list of keys to be removed
      */
-    private void removeFromObjectStore(ObjectStoreModule module, String ... keys) throws ObjectStoreException {
+    private void removeFromObjectStore(ObjectStoreModule module, String... keys) throws ObjectStoreException {
         for (String key : keys) {
             try {
                 module.getObjectStoreManager().getObjectStore(OBJECTSTORE_PARTITION).remove(key);
             } catch (ObjectDoesNotExistException e) {
-                //Do nothing if the key doesn't exist
+                // Do nothing if the key doesn't exist
             }
         }
     }
