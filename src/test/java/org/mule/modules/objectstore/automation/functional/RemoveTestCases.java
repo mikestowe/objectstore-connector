f574e41054505f90b6e3624ec4dfc4ee4b98a1e5
Sandipan Basu <sandipan@whiteskylabs.com>
2015-07-23 16:52:35 +0530

    FIxed RemoveTestCases

--- src/test/java/org/mule/modules/objectstore/automation/functional/RemoveTestCases.java
+++ src/test/java/org/mule/modules/objectstore/automation/functional/RemoveTestCases.java
@@ -10,42 +10,32 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mule.api.store.ObjectDoesNotExistException;
 import org.mule.modules.objectstore.ObjectStoreConnector;
-import org.mule.transport.NullPayload;
 
 import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertTrue;
 
 public class RemoveTestCases extends AbstractTestCase {
 
+    ObjectStoreConnector module;
+
     @Before
     public void setUp() throws Exception {
-        ObjectStoreConnector module = this.getConnector();
+        module = this.getConnector();
         module.store(OBJECTSTORE_KEY, OBJECTSTORE_VALUE, false);
     }
 
     @Test
     public void testRemove() throws Exception {
-        ObjectStoreConnector module = this.getConnector();
         String value = (String) module.remove(OBJECTSTORE_KEY, false);
         assertEquals(OBJECTSTORE_VALUE, value);
     }
 
     @Test(expected = ObjectDoesNotExistException.class)
     public void testRemoveFail() throws Exception {
-        ObjectStoreConnector module = this.getConnector();
         module.remove("muleion", false);
-    }
-
-    @Test
-    public void testRemoveNotExists() throws Exception {
-        ObjectStoreConnector module = this.getConnector();
-        Object removedObject = module.remove("muleion", false);
-        assertTrue(removedObject instanceof NullPayload);
     }
 
     @After
     public void tearDown() throws Exception {
-        ObjectStoreConnector module = this.getConnector();
         module.remove(OBJECTSTORE_KEY, true);
     }
 }
